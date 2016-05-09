72205cd88083ee53577795edea539863928024d3
KarelVerbrugge <karel.verbrugge@ugent.be>
2016-05-09 12:59:14 +0200

    Update basic-import.R

--- basic-import.R
+++ basic-import.R
@@ -32,17 +32,54 @@ AWARE <- dbConnect(MySQL(), user='username', password='password', host='awarefra
 # Add sensors and fields accordingly
 # Fetch request requires definition of n = -1 to query more than the default number of rows (500)
 
+# Create a list of available data frames for export regardless of specifics
+datalist <- c()
+
 # Foreground applications
+# Resource: http://www.awareframework.com/applications/
 
 foreground_query <- dbSendQuery(AWARE, "SELECT _id, timestamp, device_id, package_name, application_name FROM applications_foreground")
 data_foreground <- fetch(foreground_query, n = -1)
+datalist[length(datalist) + 1] <- "data_foreground"
 
 # Notifications
+# Resource: http://www.awareframework.com/applications/
 
 notifications_query <- dbSendQuery(AWARE, "SELECT _id, timestamp, device_id, package_name, application_name, text, sound, vibrate, flags FROM applications_notifications")
 data_notifications <- fetch(notifications_query, n = -1)
+datalist[length(datalist) + 1] <- "data_notifications"
 
 # Screen status
+# Resource: http://www.awareframework.com/screen/
 
 screen_query <- dbSendQuery(AWARE, "SELECT _id, timestamp, device_id, screen_status FROM screen")
 data_screen <- fetch(screen_query, n = -1)
+datalist[length(datalist) + 1] <- "data_screen"
+
+
+# PRE-PROCESSING
+################
+
+# Add readable timestamp variable
+# AWARE are based on UNIX epoch but measured in milliseconds, so divide by 1000 before conversion
+
+data_foreground$time <- as.POSIXct(data_foreground$timestamp/1000, tz = "CET", origin="1970-01-01")
+data_notifications$time <- as.POSIXct(data_notifications$timestamp/1000, tz = "CET", origin="1970-01-01")
+data_screen$time <- as.POSIXct(data_screen$timestamp/1000, tz = "CET", origin="1970-01-01")
+
+# Add readable screen status variable
+# Values are based on AWARE documentation
+
+screen_values <- c(0:3)
+screen_labels <- c("off","on","locked","unlocked")
+data_screen$status <- screen_labels[match(data_screen$screen_status, screen_values)]
+
+# Backup raw data files to .csv
+
+for (i in 1:length(datalist)) {
+  write.csv(datalist[i], file = paste0(""",datalist[i],".csv"")
+}
+
+write.csv(data_foreground, file = "dataset_foreground.csv")
+write.csv(data_notifications, file = "dataset_notifications.csv")
+write.csv(data_screen, file = "dataset_screen.csv")
